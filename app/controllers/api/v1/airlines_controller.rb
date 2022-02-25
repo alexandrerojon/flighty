9f3eac6d46753ee7a7775a96f340e89bb21cdfbb
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-25 14:01:04 +0100

    Airline create and update finished

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -10,5 +10,30 @@ module Api
         airline = Airline.find_by(slug: params[:slug])
         render json: AirlineSerializer.new(airline).serialized_json
       end
+
+      def create
+        airline = Airline.new(airline_params)
+        if airline.save
+          render json: AirlineSerializer.new(airline).serialized_json
+        else
+          render json { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+        if airline.update(airline_params)
+          render json: AirlineSerializer.new(airline).serialized_json
+        else
+          render json { error: airline.errors.messages }, status: 422
+        end
+      end
+
+
+      private
+
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
   end
 end
