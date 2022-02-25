59bc841487d694c2ebca04487d298c17204672bb
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-25 13:43:48 +0100

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
+
+  namesspace :api do
+    namesspace :v1 do
+      resources :airlines, params: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # This will re-route requests that are not for existing paths pre-defined in our API back to our index path.
+  get "*path", to: "pages#index", via: :all
 end
