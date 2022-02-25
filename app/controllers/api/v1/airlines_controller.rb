5dfd916530e8fc1eaea7824a3b962b438b67edf0
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-25 14:08:14 +0100

    private method in airlines controller added

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -3,12 +3,12 @@ module Api
     class AirlinesController < ApplicationController
       def index
         airlines = Airline.all
-        render json: AirlineSerializer.new(airlines).serialized_json
+        render json: AirlineSerializer.new(airlines, options).serialized_json
       end
 
       def show
         airline = Airline.find_by(slug: params[:slug])
-        render json: AirlineSerializer.new(airline).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
 
       def create
@@ -23,7 +23,7 @@ module Api
       def update
         airline = Airline.find_by(slug: params[:slug])
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airline).serialized_json
+          render json: AirlineSerializer.new(airline, options).serialized_json
         else
           render json { error: airline.errors.messages }, status: 422
         end
@@ -40,9 +40,12 @@ module Api
       end
 
       private
-
       def airline_params
         params.require(:airline).permit(:name, :image_url)
+      end
+
+      def options
+        @options ||= { include: %i[reviews] }
       end
   end
 end
