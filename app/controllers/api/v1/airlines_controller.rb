f7c9355209a3c8be71ed1cf3c6d896a6d877a1ab
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-02-25 14:25:47 +0100

    airlines controller bug -fixes / typos

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class AirlinesController < ApplicationController
+
       def index
         airlines = Airline.all
         render json: AirlineSerializer.new(airlines, options).serialized_json
@@ -10,13 +11,12 @@ module Api
         airline = Airline.find_by(slug: params[:slug])
         render json: AirlineSerializer.new(airline, options).serialized_json
       end
-
       def create
         airline = Airline.new(airline_params)
         if airline.save
           render json: AirlineSerializer.new(airline).serialized_json
         else
-          render json { error: airline.errors.messages }, status: 422
+          render json: { error: airline.errors.messages }, status: 422
         end
       end
 
@@ -25,27 +25,29 @@ module Api
         if airline.update(airline_params)
           render json: AirlineSerializer.new(airline, options).serialized_json
         else
-          render json { error: airline.errors.messages }, status: 422
+          render json: { error: airline.errors.messages }, status: 422
         end
       end
-
 
       def destroy
         airline = Airline.find_by(slug: params[:slug])
         if airline.destroy(airline_params)
           head :no_content
         else
-          render json { error: airline.errors.messages }, status: 422
+          render json: { error: airline.errors.messages }, status: 422
         end
       end
+
 
       private
       def airline_params
         params.require(:airline).permit(:name, :image_url)
       end
-
       def options
         @options ||= { include: %i[reviews] }
       end
+
+
+    end
   end
 end
